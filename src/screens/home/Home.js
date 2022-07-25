662d421fadc0c4d0e4c46ad611416d578c50eed9
karanb <karan.bhatia@dunzo.in>
2022-07-25 18:49:25 +0530

    add logo and and add content to home screen

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,8 +1,21 @@
 import React from "react";
+import Header from "../../common/header/Header";
+import "./Home.css";
 
 export default function Home(){
-    return(
-    <div>
-        Home
-    </div>);
+   return(
+    <div className="home-container">
+        <Header />
+        <div className="home-content">
+            <div className="home-content-text">
+                <h1>Welcome to Movie Booking System</h1>
+                <p>
+                    This is a movie booking system that allows you to book tickets for movies.
+                    You can book tickets for movies in your city.
+                </p>
+            </div>
+            
+        </div>
+    </div>
+   );
 }
